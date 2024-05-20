880fdc0afd1a002fcc8b5a772ca69e4bea7b834a
Sean B <strawhorse@gmail.com>
2024-05-20 14:50:27 +0100

    Some errors in opening program on different IDE
    Resolved

--- src/com/Jbr/networkChat/MultithreadedSimpleServer.java
+++ src/com/Jbr/networkChat/MultithreadedSimpleServer.java
@@ -11,44 +11,35 @@ public class MultithreadedSimpleServer {
     public static void main(String[] args) throws IOException {
 
         //    just echo back to client what it sends
-        ServerSocket serverSocket = new ServerSocket(5000);
-
-
-        while(true) {
-
-            Socket socket = serverSocket.accept();
-
-            System.out.println("Server now accepting client connections...");
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-
-            socket.setSoTimeout(20000);
-
-//        set up a buffered reader for the input from the client
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-//      Set up output writer for the output to the client after being sent to the server; and autoflush = true
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-
-//        listen for input
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
 
             while (true) {
-                String stringToBeEchoed = reader.readLine();
 
-                System.out.println("Server received the text: " + stringToBeEchoed);
+                Socket socket = serverSocket.accept();
 
-                if(stringToBeEchoed.equals("exit")){
-                    break;
+                System.out.println("Server now accepting client connections...");
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
+                socket.setSoTimeout(20000);
+
+                while (true) {
+                    String stringToBeEchoed = reader.readLine();
+
+                    System.out.println("Server received the text: " + stringToBeEchoed);
+
+                    if (stringToBeEchoed.equals("exit")) {
+                        break;
+                    }
+
+                    writer.println("Message back from server: " + stringToBeEchoed);
                 }
-
-                writer.println("Message back from server: " + stringToBeEchoed);
             }
         }
-
+        catch (IOException e) {
+            System.out.println("Error arose, error: " + e);
+        }
     }
-
-
 }
